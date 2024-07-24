4807fe9946b0c420ff86205b13d256f6212dd15c
alessandro-barate <161752327+aleek89@users.noreply.github.com>
2024-07-24 22:11:21 +0200

    [UPD]

--- database/migrations/2024_07_24_200624_create_types_table.php
+++ database/migrations/2024_07_24_200624_create_types_table.php
@@ -13,7 +13,8 @@ return new class extends Migration
     {
         Schema::create('types', function (Blueprint $table) {
             $table->id();
-            $table->string('title', 30);
+            $table->string('title', 30)->unique();
+            $table->string('slug', 25);
             $table->timestamps();
         });
     }
