199d6379b80838bad5b9bbeac0ea2d7e900e7a0a
alessandro-barate <161752327+aleek89@users.noreply.github.com>
2024-07-25 14:59:58 +0200

    [UPD]

--- database/seeders/PostSeeder.php
+++ database/seeders/PostSeeder.php
@@ -7,6 +7,7 @@ use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 use Faker\Generator as Faker;
 use Illuminate\Support\Str;
+use Illuminate\Support\Arr;
 
 class PostSeeder extends Seeder
 {
@@ -19,11 +20,14 @@ class PostSeeder extends Seeder
         for($i = 0; $i < 100; $i++) {
 
             $post = new Post();
+            // $types = ['Frontend', 'Backend', 'Data Analytics', 'Cloud', 'Machine Learning', 'Fullstack', 'System Administrator', 'Firmware Development', 'App Development', 'Cybersecurity'];
 
             // Assegno valori specifici alle colonne
             $post->title = $faker->sentence(3);
             $post->content = $faker->text(500);
             $post->slug = Str::of($post->title)->slug('-');
+
+            // $post->type_id = Arr::random($types['id']);
 
             // Salva il dato in tabella
             $post->save();
